24019d90aae245b99391b9f8a0e706ff1b4d8637
Trevor Yates <travelingtrevor123@gmail.com>
2023-09-16 13:36:35 -0500

    Changed to using .env for environment vars

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
+from helpers import settings
 from web import socket, app
 import os
 
 if __name__ == "__main__":
-	socket.run(app, host="127.0.0.1", port=5000, debug=bool(os.getenv("DEBUG")))
+	socket.run(app, host="127.0.0.1", port=5000, debug=bool(settings.ENV["DEBUG"]))
