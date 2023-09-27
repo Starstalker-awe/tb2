ed0066f68e4ec2adc149e8cb849862c659ef697b
Trevor Yates <travelingtrevor123@gmail.com>
2023-09-27 11:02:47 -0500

    Renamed to Subprocess, and converted Subprocess.stoppable to a generator

--- helpers/classes.py
+++ helpers/classes.py
@@ -1,15 +1,18 @@
 import multiprocessing
 
-class Stoppable:
+class Subprocess:
 	def __init__(self):
 		self.tasks: list[multiprocessing.Process] = []
 
-	def stoppable(self, strtr, stpr, f, *a, **ka):
-		self.tasks.append(multiprocessing.Process(target=f, args=a, kwargs=ka))
-		id = len(self.tasks) - 1
+	def stoppable(self, f):
+		def __handler(strtr, stpr, *a, **ka):
+			self.tasks.append(multiprocessing.Process(target=f, args=a, kwargs=ka))
+			id = len(self.tasks) - 1
 
-		strtr.wait()
-		self.tasks[id].start()
+			strtr.wait()
+			self.tasks[id].start()
 
-		stpr.wait()
-		self.tasks[id].terminate()
+			stpr.wait()
+			self.tasks[id].terminate()
+
+		return __handler
