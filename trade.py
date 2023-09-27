bd77e6b31b09e788797eb3b360b9c722d3bbfcc0
Trevor Yates <travelingtrevor123@gmail.com>
2023-09-27 14:02:20 -0500

    Added parent linking, removed Watcher.GapChecker, added placeholder for buy/sell rules, modified Trader.generate

--- trade.py
+++ trade.py
@@ -2,6 +2,7 @@ from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
 from alpaca.data.requests import StockBarsRequest
 
 from dotmap import DotMap
+import multiprocessing
 import socketio
 import threading
 import datetime
@@ -18,7 +19,7 @@ logging.getLogger(__name__).setLevel(helpers.settings.LOGGING_LEVEL)
 
 
 class Watcher(helpers.classes.Subprocess):
-	def __init__(self, ticker: str, i: int, trigger: threading.Event, killer: threading.Event, interval = 1):
+	def __init__(self, ticker: str, i: int, trigger: threading.Event, killer: threading.Event, Wlist, interval = 1):
 		helpers.classes.Subprocess.__init__(self)
 
 		self.today = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
@@ -35,6 +36,8 @@ class Watcher(helpers.classes.Subprocess):
 		self.starter = threading.Event()
 		self.stopper = threading.Event()
 
+		Wlist[i] = self
+
 		if not self.chart.caught_up.is_set():
 			logging.debug(f'\t\tWatcher {self.uid}: Waiting to catch up with chart')
 			self.chart.caught_up.wait()
@@ -43,16 +46,40 @@ class Watcher(helpers.classes.Subprocess):
 		trigger.wait()
 
 		if datetime.datetime.utcnow() < datetime.datetime(*self.today, 9 + 4, 30, 1): 
-			logging.debug(f'\t\tWatcher {self.uid}: Starting gap checker')
-			self.GapCheck(self.chart, self.yesterday, self.starter)
+			threading.Thread(target=self.gap_check, args=[self.yesterday]).start()
 
-		
+		threading.Thread(target=self.stoppable(self.buy_rules), args=[self.starter, killer]).start()
+		threading.Thread(target=self.stoppable(self.sell_rules), args=[self.starter, killer]).start()
+		threading.Thread(target=self.starter_f).start()
 
 
-	def run(self):
+	def gap_check(self, yesterday: list[int]):
+		logging.debug(f'\t\tWatcher {self.uid}: Starting gap checker')
+		yclose = DotMap(helpers.settings.HIST_DATA_CLIENT.get_stock_bars(StockBarsRequest(
+			symbol_or_symbols=self.ticker,
+			start=datetime.datetime(*yesterday, 3 + 12 + 4, 59, 0, tzinfo=datetime.timezone.utc),
+			end=datetime.datetime(*yesterday, 4 + 12 + 4, 0, 0, tzinof=datetime.timezone.utc),
+			timeframe=TimeFrame(amount=1, unit=TimeFrameUnit('Min'))
+		))).data[self.ticker][0].close
+
+		if self.chart.current() == yclose: return
+		elif self.chart.current() > yclose:
+			pass # Short order at self.chart.bars[0].open() - .01
+		else:
+			pass # Short order at market
+
+
+	def buy_rules(self):
+		logging.debug(f'\t\tWatcher {self.uid}: Starting buy rules runner')
 		while True:
 			# Code here
-			time.sleep(1)
+			time.sleep(self.interval)
+
+	def sell_rules(self):
+		logging.debug(f'\t\tWatcher {self.uid}: Starting sell rules runner')
+		while True:
+			# Code here
+			time.sleep(self.interval)
 
 	def starter_f(self):
 		time.sleep(26 * 60)
@@ -61,53 +88,33 @@ class Watcher(helpers.classes.Subprocess):
 
 	def pass_data(self, data: helpers.charts.Datapoint): self.chart.add(data)
 
-	class GapCheck(helpers.classes.Subprocess):
-		def __init__(self, chart: helpers.charts.Chart, yesterday: datetime.datetime, pstart: threading.Event):
-			helpers.classes.Subprocess.__init__(self)
 
-			self.starter = threading.Event()
-			self.stopper = threading.Event()
-
-			self.chart = chart
-			self.yesterday = yesterday
-			self.direction = chart.open() > (yclose := DotMap(helpers.settings.HIST_DATA_CLIENT.get_stock_bars(StockBarsRequest(
-				symbol_or_symbols=self.ticker,
-				start=datetime.datetime(*yesterday, 3 + 12 + 4, 59, 0, tzinfo=datetime.timezone.utc),
-				end=datetime.datetime(*yesterday, 4 + 12 + 4, 0, 0, tzinof=datetime.timezone.utc),
-				timeframe=TimeFrame(amount=1, unit=TimeFrameUnit('Min'))
-			))).data[self.ticker][0].close)
-
-			self.starter.wait() # Global starter
-
-			threading.Thread(target=self.stoppable(self.run), args=[self.starter, self.stopper]).start()
-			threading.Thread(target=self.starter_f).start()
-			threading.Thread(target=self.timeout).start()
-
-		def run(self): # Runs 1 minute after open
-			while True:
-				if self.direction: # Gapped up
-					pass
-				else: # Gapped down
-					pass
-				time.sleep(1)
-
-		def timeout(self):
-			time.sleep(13 * 60)
-			self.stopper.set()
-
-		def starter_f(self):
-			time.sleep(60)
-			self.starter.set()
 
 
 class WatcherList:
-	def __init__(self, tickers: list[str], starter: threading.Event, killers):
-		self.watchers = list[Watcher] = [None] * len(tickers)
+	def __init__(self, tickers: list[str], starter: threading.Event, killers: list[threading.Event], WLlist, id: int):
+		
+		self._threads: list[threading.Thread] = [threading.Thread] * len(tickers)
+		self.watchers: list[Watcher] = list[Watcher] * len(tickers)
 
 		for i, ticker in enumerate(tickers):
-			self.watchers[i] = Watcher(ticker, i, starter, killers[ticker])
+			self._threads[i] = threading.Thread(
+				target=Watcher, 
+				args=[
+					ticker, 
+					i, 
+					starter, 
+					killers[ticker],
+					self.watchers
+				]
+			)
+			self._threads[i].start()
 
-	def tickers(self): list(map(lambda t:t.ticker, self.watchers))
+		WLlist[id] = self
+
+	def tickers(self): list(map(lambda t:t.ticker, self._threads))
+
+
 
 
 class Trader:
@@ -119,8 +126,20 @@ class Trader:
 		self.ws = socketio.Client()
 
 	def generate(self):
-		self._threads = [None] * (c := math.ceil(len(self.stocks) / CPU_THREADS))
-		for i in range(c): self._threads[i] = threading.Thread(target=WatcherList, args=(self.stocks[i*c:(i+1)*c],self.state,))
+		self._threads = [multiprocessing.Process] * (c := math.ceil(len(self.stocks) / CPU_THREADS))
+		self.watcherlists: list[WatcherList] = [WatcherList] * c
+		
+		for i in range(c):
+			self._threads[i] = multiprocessing.Process(
+				target=WatcherList, 
+				args=[
+					self.stocks[i*c:(i+1)*c], 
+					self.state, 
+					self.killers, 
+					self.watcherlists, 
+					i
+				]
+			)
 
 		self.setup_socket_routes(self.ws)
 		self.ws.connect("ws://127.0.0.1:5000", namespaces='/internal')
@@ -139,5 +158,8 @@ class Trader:
 			self.killers[ticker] = threading.Event()
 
 
-	def kill_watcher(ticker: str):
-		pass
+	def kill_watcher(self, ticker: str):
+		k = self.killers.get(ticker)
+		if k:
+			k.set()
+			del self.killers[ticker]
